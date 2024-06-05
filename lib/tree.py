4f989d5c84740209e19c606d1bd8abca85392789
omolo-j <omoloj135@gmail.com>
2024-06-05 17:36:25 +0300

    dsa-tree-traversal

--- lib/tree.py
+++ lib/tree.py
@@ -1,6 +1,20 @@
 class Tree:
-  def __init__(self, root = None):
-    self.root = root
+    def __init__(self, root=None):
+        self.root = root
 
-  def get_element_by_id(self, id):
-    pass
+    def get_element_by_id(self, id):
+        return self._search_by_id(self.root, id)
+
+    def _search_by_id(self, node, id):
+        if node is None:
+            return None
+        
+        if node.get('id') == id:
+            return node
+        
+        for child in node.get('children', []):
+            result = self._search_by_id(child, id)
+            if result is not None:
+                return result
+        
+        return None
